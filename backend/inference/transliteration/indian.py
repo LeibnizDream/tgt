10eb6c4cc811107ee6b6f652ce73bb01c53d98b9
Alejandra Camelo Cruz <camelo.cruz@leibniz-zas.de>
2025-11-21 10:38:01 +0100

    Update indian.py

--- backend/inference/transliteration/indian.py
+++ backend/inference/transliteration/indian.py
@@ -6,7 +6,7 @@ from indic_transliteration.sanscript import transliterate
 class BanglaStrategy(TransliterationStrategy):
     def __init__(self):
         self.script_from = sanscript.BENGALI
-        self.script_to = sanscript.IAST  # THIS is the academic Latin output you want
+        self.script_to = sanscript.IAST
 
     def transliterate(self, text: str) -> str:
         return transliterate(text, self.script_from, self.script_to)
