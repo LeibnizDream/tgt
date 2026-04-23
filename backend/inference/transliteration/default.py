05c16ad9666b6925e6c0b7aab47bba12861b4c78
Alejandra Camelo Cruz <135133679+camelo-cruz@users.noreply.github.com>
2026-04-23 16:59:42 +0200

    changing text in greek

--- backend/inference/transliteration/default.py
+++ backend/inference/transliteration/default.py
@@ -3,4 +3,9 @@ from transliterate import translit
 
 class DefaultStrategy(TransliterationStrategy):
     def transliterate(self, sentence: str) -> str:
-        return translit(sentence, self.language_code, reversed=True)
+        text = translit(sentence, self.language_code, reversed=True)
+        if self.language_code == 'el':
+            text = text.replace('x', 'ks').replace('y', 'u')
+        
+        return text
+            
