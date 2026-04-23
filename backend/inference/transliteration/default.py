5973d259d5853532f00e2fd4deab4f2cf4d5d850
Alejandra Camelo Cruz <135133679+camelo-cruz@users.noreply.github.com>
2026-04-23 18:57:03 +0200

    Update default.py

--- backend/inference/transliteration/default.py
+++ backend/inference/transliteration/default.py
@@ -1,11 +1,14 @@
-from inference.transliteration.abstract import TransliterationStrategy 
+import re
+
+from inference.transliteration.abstract import TransliterationStrategy
 from transliterate import translit
 
 class DefaultStrategy(TransliterationStrategy):
     def transliterate(self, sentence: str) -> str:
         text = translit(sentence, self.language_code, reversed=True)
         if self.language_code == 'el':
-            text = text.replace('x', 'ks').replace('y', 'u')
-        
+            text = re.sub(r'\w+@pii|x', lambda m: m.group() if '@pii' in m.group() else 'ks', text)
+            text = text.replace('y', 'u')
+
         return text
             
