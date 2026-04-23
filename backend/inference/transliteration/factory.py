ff5b2a56bfe44e792c3879e0235e016b099d1b9e
Alejandra Camelo Cruz <camelo.cruz@leibniz-zas.de>
2026-04-23 12:41:23 +0200

    adjusting transliteration

--- backend/inference/transliteration/factory.py
+++ backend/inference/transliteration/factory.py
@@ -2,6 +2,7 @@ from inference.transliteration.abstract import TransliterationStrategy
 from inference.transliteration.japanese import JapaneseStrategy
 from inference.transliteration.chinese import ChineseStrategy
 from inference.transliteration.bengali import BengaliStrategy
+from inference.transliteration.default import DefaultStrategy
 
 class TransliterationStrategyFactory:
     @staticmethod
@@ -12,6 +13,8 @@ class TransliterationStrategyFactory:
             return JapaneseStrategy()
         elif language_code == "bn":
             return BengaliStrategy()
+        elif language_code in ['el', 'ru']:
+            return DefaultStrategy(language_code=language_code)
         else:
             raise ValueError(f"No transliteration strategy available for language code: {language_code}")
             
