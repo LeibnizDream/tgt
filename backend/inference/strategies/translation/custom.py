e891d8f8a8703d6c8a77aa3c953a1dbf5467b0af
Alejandra Camelo Cruz <camelo.cruz@leibniz-zas.de>
2026-05-11 08:48:30 +0200

    Update custom.py

--- backend/inference/strategies/translation/custom.py
+++ backend/inference/strategies/translation/custom.py
@@ -4,7 +4,7 @@ from inference.strategies.translation.M2M100 import M2M100Strategy
 class CustomTranslationStrategy(M2M100Strategy):
 
     def __init__(self, language_code: str, model_name: str):
-        self.model_name = model_name  # set before super().__init__ calls load_model()
+        self.model_name = model_name
         super().__init__(language_code)
 
     def load_model(self) -> None:
